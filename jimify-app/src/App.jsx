e4122e8abb17590b428309ce10cfd2e8dfbb2cf6
pekyiheng <yihengpek@gmail.com>
2025-05-25 20:36:16 +0700

    Add header file

--- jimify-app/src/App.jsx
+++ jimify-app/src/App.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className='app'>
-      <NavBar />
+      <Header />
       <Outlet />
+      <NavBar />
     </div>
   )
 }
---
+++ jimify-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Jimify</h1>
+        </div>
+    )
+}
+
+export default Header;
