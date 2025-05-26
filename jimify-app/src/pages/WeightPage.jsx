37315a4c0fb2bac60d92853d1e7ce28584d11613
Fu Xiang <orfuxiang2015@gmail.com>
2025-05-26 15:09:30 +0800

    testing

--- jimify-app/src/pages/WeightPage.jsx
+++ jimify-app/src/pages/WeightPage.jsx
@@ -1,11 +1,38 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const WeightPage = () => {
+    const [weight, setWeight] = useState("XX");
+    const [lastUpdated, setLastUpdated] = useState("XX");
+    const [oldEntries, setEntries] = useState([]);
+    
+    const handlePrompt = () => {
+        const value = prompt("New Entry:");
+        setWeight(value);
+        
+        const today = new Date().toLocaleDateString("en-GB", {day: "2-digit", month: "short", year: "numeric"});
+        setLastUpdated(today);
+
+        const newEntry = value + "KG, " + today;
+        setEntries([...oldEntries, newEntry]);
+    }
+
     return (
-        <div>Weight
+        <div>
+            <h1>{weight} KG</h1>
+            <h2>Last Updated: {lastUpdated}</h2>
+            
+            <div>
+                <ul>
+                    {oldEntries.map(entry => (<li>{entry}</li>))}
+                </ul>
+            </div>
+            
+            <div>
+                <button onClick={handlePrompt}>+ New Entry</button>
+            </div>
             <Outlet/>
         </div>
-        
     )
 }
 
