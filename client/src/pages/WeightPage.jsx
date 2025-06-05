d95596eedc871c33a76aaf7f3760ed2400744013
Fu Xiang <orfuxiang2015@gmail.com>
2025-06-05 17:51:10 +0800

    Add chart to weights

--- client/src/pages/WeightPage.jsx
+++ client/src/pages/WeightPage.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { collection, getDoc, getDocs, addDoc, setDoc, deleteDoc, doc, query, where } from "firebase/firestore";
 import { db, auth } from "../firebase_config"
 import { onAuthStateChanged } from "firebase/auth";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 const WeightPage = () => {
 
@@ -24,12 +26,12 @@ const WeightPage = () => {
         try {
             const docSnap = await getDocs(userWeightColRef);
             const data = docSnap.docs.map((docSnap) => {
-            const docData = docSnap.data();
-            return {
-                id: docSnap.id,
-                value: docData.value,
-                time: docData.time.toDate(),
-            };
+                const docData = docSnap.data();
+                return {
+                    id: docSnap.id,
+                    value: docData.value,
+                    time: docData.time.toDate(),
+                };
         });
 
         setWeight(data);
@@ -42,7 +44,7 @@ const WeightPage = () => {
     }
 
     const handleNewWeight = async () => {
-        const input = prompt("New Entry:");
+        const input = prompt("New Entry (KG):");
         const value = parseFloat(input);
         
         if (!isNaN(value)) {
@@ -86,6 +88,29 @@ const WeightPage = () => {
         );
     }
 
+    ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+    const chartData = {
+        labels: oldWeight.map(entry => entry.time.toLocaleDateString("en-GB", { day: "2-digit", month: "short"})),
+        datasets: [
+            {
+                label: 'Weight (KG)',
+                data: oldWeight.map(entry => entry.value),
+                fill: false,
+                borderColor: 'rgb(24, 22, 172)',
+                tension: 0
+            }
+        ]
+    }
+
+    const chartOptions = {
+        responsive: true,
+        plugins: {
+            legend: { position: 'top' },
+            title: { display: true, text: 'Weight Over Time' }
+        }
+    }
+
     return (
         <div className="weightContainer">
             {oldWeight.length > 0 ? (
@@ -95,16 +120,20 @@ const WeightPage = () => {
                 </>)
                 : <h2>No Entries Yet</h2>
             }
+
+            <div className="chartContainer">
+                <Line data={chartData} options={chartOptions} />
+            </div>
             
+            <div>
+                <button onClick={handleNewWeight}>+ New Entry</button>
+            </div>
+
             <div>
                 <ul className="verticalListOfBoxes">
                     {oldWeight.map((entry, index) => 
                         (<li className="listItemInBox" key={entry.id}><WeightEntry weight={entry.value} time={entry.time} onDelete={() => handleDeleteWeight(entry.id)}/></li>))}
                 </ul>
-            </div>
-            
-            <div>
-                <button onClick={handleNewWeight}>+ New Entry</button>
             </div>
         </div>
     )
