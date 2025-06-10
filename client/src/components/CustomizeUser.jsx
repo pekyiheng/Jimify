f5ba93f5be1e5b65539d16d7aa784d8ef299db9a
yihengpek <yihengpek@gmail.com>
2025-06-10 08:54:03 +0000

    Save data entered in CustomizeUser to database

--- client/src/components/CustomizeUser.jsx
+++ client/src/components/CustomizeUser.jsx
@@ -1,15 +1,51 @@
 import { useState, useEffect, useRef } from 'react';
+import { db, auth } from "../firebase_config"
+import { onAuthStateChanged } from "firebase/auth";
+import { getDoc, setDoc, doc, } from "firebase/firestore";
 
 const CustomizeUser = () => {
 
     const [showDialog, setShowDialog] = useState(false);
     const dialogRef = useRef(null);
+    const [username, setUsername] = useState('');
     const [dailyCalories, setDailyCalories] = useState(0);
     const [weight, setWeight] = useState(0);
     const [height, setHeight] = useState(0);
     const [age, setAge] = useState(0);
     const [gender, setGender] = useState('M');
-    const [activityLevel, setActivityLevel] = useState(1.2);
+    const [activityLevel, setActivityLevel] = useState('sedentary');
+    const [goal, setGoal] = useState('maintain');
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                fetchUserProfile(user.uid);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    const fetchUserProfile = async (uid) => {
+        const userProfileDocRef = doc(db, "Users", uid);
+        try {
+            const docSnap = await getDoc(userProfileDocRef);
+            if (docSnap.exists() && docSnap.data().Username) {
+                setUsername(docSnap.data().Username);
+                docSnap.data().Gender && setGender(docSnap.data().Gender);
+                docSnap.data().Age && setAge(docSnap.data().Age);
+                docSnap.data().Height && setHeight(docSnap.data().Height);
+                docSnap.data().Weight && setWeight(docSnap.data().Weight);
+                docSnap.data().Goal && setGoal(docSnap.data().Goal);
+                docSnap.data().Activity_Level && setActivityLevel(docSnap.data().Activity_Level);
+                docSnap.data().Daily_Calories && setDailyCalories(docSnap.data().Daily_Calories);
+            } else {
+                setShowDialog(true);
+            }
+            
+        } catch (e) {
+            console.error("Error fetching food list:", e);
+        }
+    }
 
     const toggleDialog = () => {
         setShowDialog(!showDialog);
@@ -17,9 +53,9 @@ const CustomizeUser = () => {
 
     useEffect(() => {
         if (showDialog) {
-        dialogRef.current.showModal();
+            dialogRef.current.showModal();
         } else {
-        dialogRef.current.close();
+            dialogRef.current.close();
         }
     }, [showDialog]);
 
@@ -34,65 +70,92 @@ const CustomizeUser = () => {
 
     }
 
-    const toggleActivityLevel = (e) => {
-        switch (e.target.value) {
-            case 'light':
-                setActivityLevel(1.375);
-                break;
-            case 'moderate':
-                setActivityLevel(1.55);
-                break;
-            case 'active':
-                setActivityLevel(1.725);
-                break;
-            case 'very active':
-                setActivityLevel(1.9);
-                break;
+    const getGoal = (goal) => {
+        switch (goal) {
+            case 'gain fast':
+                return 500;
+            case 'gain slow':
+                return 200;
+            case 'lose slow':
+                return (-200);
+            case 'lose fast':
+                return (-500);
             default:
-                setActivityLevel(1.2);
+                return 0; //maintain
+        }
+    }
+
+    const getActivityLevel = (activity) => {
+        switch (activity) {
+            case 'light':
+                return (1.375);
+            case 'moderate':
+                return(1.55);
+            case 'active':
+                return (1.725);
+            case 'very active':
+                return(1.9);
+            default:
+                return (1.2); //sedentary
         }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setDailyCalories(calculateBMR(gender, weight, height, age, activityLevel));
+        const dailyGoal = Math.round(calculateBMR(gender, weight, height, age, getActivityLevel(activityLevel)) + getGoal(goal));
+        setDailyCalories(dailyGoal);
+        const userProfileDocRef = doc(db, "Users", auth.currentUser.uid);
+        setDoc(userProfileDocRef, {
+            Username: username,
+            Daily_Calories: dailyGoal,
+            Weight: weight,
+            Height: height,
+            Age: age,
+            Gender: gender,
+            Activity_Level: activityLevel,
+            Goal: goal,
+        }, {merge: true});
+
     }
 
     return (
         <>
-            <button onClick={toggleDialog}>Show dialog</button>
+            <button onClick={toggleDialog}>Edit profile</button>
             <dialog ref={dialogRef}>
-                <header>
-                    Welcome to Jimify! Let's set up yout profile
+                <header className='registerHeader'>
+                    <h2>Welcome to Jimify! Let's set up yout profile </h2>
                 </header>
                 <div>
                     <form onSubmit={handleSubmit}>
                         <label htmlFor='usernameField' >Username</label>
-                        <input id='usernameField' type='text'></input>
+                        <input required id='usernameField' type='text' value={username} onChange={e => setUsername(e.target.value)}></input>
                         <br></br>
-                        <label htmlFor='genderField' >What is your age?</label>
-                        <select id='genderField' value={gender} onChange={e => setGender(e.target.value)} >
+                        <label htmlFor='genderField' >What is your gender?</label>
+                        <select required id='genderField' value={gender} onChange={e => setGender(e.target.value)} >
                             <option value='M' >Male</option>
                             <option value='F' >Female</option>
                         </select>
                         <br></br>
                         <label htmlFor='ageField' >What is your age?</label>
-                        <input id='ageField' type='number' onChange={e => setAge(e.target.value)} ></input>
+                        <input required id='ageField' type='number' value={age} onChange={e => setAge(e.target.value)} ></input>
                         <br></br>
                         <label htmlFor='heightField' >What is your height?</label>
-                        <input id='heightField' type='number' onChange={e => setHeight(e.target.value)} ></input>
+                        <input required id='heightField' type='number' value={height} onChange={e => setHeight(e.target.value)} ></input>
                         <br></br>
                         <label htmlFor='initialWeightField' >What is your current weight?</label>
-                        <input id='initialWeightField' type='number' onChange={e => setWeight(e.target.value)} ></input>
+                        <input required id='initialWeightField' type='number' value={weight} onChange={e => setWeight(e.target.value)} ></input>
                         <br></br>
                         <label htmlFor='goalField' >What is your goal?</label>
-                        <select id="goalField" name="goalField">
-                            <option value="gain">Gain weight</option>
-                            <option value="lose">Lose weight</option>
+                        <select required id="goalField" name="goalField" value={goal} onChange={(e) => setGoal(e.target.value)}>
+                            <option value="gain fast">Gain weight (fast)</option>
+                            <option value="gain slow">Gain weight (slow)</option>
                             <option value="maintain">Maintain weight</option>
+                            <option value="lose slow">Lose weight (slow)</option>
+                            <option value="lose fast">Lose weight (fast 0.5kg/week)</option>
                         </select>
-                        <label htmlFor='goalField' >How active are you??</label>
-                        <select id="activityField" name="activityField" onChange={toggleActivityLevel} >
+                        <br></br>
+                        <label htmlFor='activityField' >How active are you??</label>
+                        <select required id="activityField" name="activityField" value={activityLevel} onChange={(e) => setActivityLevel(e.target.value)} >
                             <option value="sedentary">Sedentary</option>
                             <option value="light">Lightly active</option>
                             <option value="moderate">Moderately active</option>
