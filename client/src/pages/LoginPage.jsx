8aece2f2f8005f1f9ea6099a73e5480a3e6e31d7
yihengpek <yihengpek@gmail.com>
2025-06-25 01:56:49 +0000

    Enable Google sign in

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { Link , useNavigate} from "react-router-dom";
 import '../App.css';
 import { auth } from '../firebase_config'; 
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signInAnonymously, signInWithPopup, GoogleAuthProvider  } from "firebase/auth";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import GoogleButton from "react-google-button";
 
 const LoginPage = () => {
 
@@ -13,6 +14,24 @@ const LoginPage = () => {
     const [hiddenTag, setHiddenTag] = useState(true);
     const [errorMessage, setErrorMessage] = useState("");
     const navigate = useNavigate();
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            console.log(user.uid);
+            navigate("/");
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+        });
+        navigate("/");
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -60,7 +79,16 @@ const LoginPage = () => {
 
                 <p hidden={hiddenTag} id="userNotFoundMessage">{errorMessage}</p>
             </form>
+            
             <label>New here? </label><Link to="/registerPage">Create an account</Link>
+            <div className="OrLineBreak">
+                <hr></hr>
+                <span> Or </span> 
+                <hr></hr>
+            </div>
+            <button style={{padding: "3px"}} onClick={handleGoogleSignIn}>
+                <GoogleButton className="button"/>
+            </button>
         
         </div>
     )
