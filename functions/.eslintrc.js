263d087ad55f6916e23fbbb67d437348fe5de3c9
yihengpek <yihengpek@gmail.com>
2025-07-13 03:42:44 +0000

    Migrated backend api to functions folder To allow firebase to host backend server

---
+++ functions/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "new-cap": ["error", {
+      newIsCap: true,
+      capIsNew: false,
+      capIsNewExceptions: ["Router"],
+      properties: false,
+    }],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const fatsecretRoutes = require("./routes/fatsecretRoutes");
+
+const app = express();
+const allowedOrigins = [
+  "jimify-a6795.web.app",
+  "https://jimify-a6795.firebaseapp.com/",
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+}));
+
+app.use(express.json());
+app.use("/fatsecret", fatsecretRoutes);
+
+exports.api = functions.https.onRequest(app);
