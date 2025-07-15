59c097e0fd9cd622edd39c70c8908becf0854983
yihengpek <yihengpek@gmail.com>
2025-07-15 11:58:40 +0000

    Estimate calories using AI Use Gemini AI to estimate calories in image.

--- client/src/firebase_config.js
+++ client/src/firebase_config.js
@@ -2,8 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
 
-/*
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,23 +14,13 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-*/
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDLX_Fh6KhIbxaRRICPX3Io1IcCPxtOtRk",
-  authDomain: "jimify-a6795.firebaseapp.com",
-  projectId: "jimify-a6795",
-  storageBucket: "jimify-a6795.firebasestorage.app",
-  messagingSenderId: "1072480071594",
-  appId: "1:1072480071594:web:d065f26a2689a5b3c145d3",
-  measurementId: "G-E0QK5765R5"
-}
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
+const ai = getAI(app, { backend: new GoogleAIBackend() });
+const model = getGenerativeModel(ai, { model: "gemini-2.5-flash" });
 
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { auth, db, storage, model };
--- client/src/pages/CaloriesPage.jsx
+++ client/src/pages/CaloriesPage.jsx
@@ -1,19 +1,65 @@
 import React from 'react';
 import AddFood from "../components/AddFood";
 import { formatDateToYYYYMMDD } from "../helper";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getDoc, doc, } from "firebase/firestore";
-import { db } from "../firebase_config"
+import { db, model } from "../firebase_config"
 import { useUser } from "../UserContext";
+
 
 const CaloriesPage = () => {
     const [totalCalories, setTotalCalories] = useState(0);
     const [curDate, setCurDate] = useState(new Date());
+    const [imageFile, setImageFile] = useState(null);
+    const [showDialog, setShowDialog] = useState(false);
+    const [showErr, setShowErr] = useState(false);
+    const [aiResponse, setAiResponse] = useState('');
+    const dialogRef = useRef(null);
     const { userId } = useUser();
+
+    useEffect(() => {
+        if (showDialog) {
+            dialogRef.current.showModal();
+        } else {
+            dialogRef.current.close();
+        }
+    }, [showDialog]);
 
     useEffect(() => {
         fetchCalories(userId)
     }, [curDate])
+
+    const promptAI = async () => {
+        if (imageFile === null) {
+            setShowErr(true);
+            return;
+        }
+
+        const prompt = "Can you estimate how much calories is in the food? Give me a definite number."
+        
+        const imagePart = await fileToGenerativePart(imageFile);
+        console.log("prompting...")
+  
+    // To generate text output, call generateContent with the text and image
+        const result = await model.generateContent([prompt, imagePart]);
+  
+        const response = result.response;
+        const text = response.text();
+        setAiResponse(text);
+    }
+
+    // Converts a File object to a Part object.
+async function fileToGenerativePart(file) {
+    const base64EncodedDataPromise = new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result.split(',')[1]);
+      reader.readAsDataURL(file);
+    });
+    return {
+      inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+    };
+  }
+  
 
     const fetchCalories = async (uid) => {
         const userCaloriesDocRef = doc(db, "Users", uid, "User_Calories", formatDateToYYYYMMDD(curDate));
@@ -38,9 +84,27 @@ const CaloriesPage = () => {
         fetchCalories(userId);
     }
 
+    const cancelAIDialog = () => {
+        setShowErr(false);
+        setShowDialog(false);
+        setAiResponse('');
+        setImageFile(null);
+    }
+
 
     return (
         <div>            
+            <dialog ref={dialogRef}>
+                <input 
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setImageFile(e.target.files[0])}
+                />
+                <p className='invalidFields'>{showErr ? "Please select an image" : ""}</p>
+                <p>{aiResponse}</p>
+                <button onClick={promptAI} >Scan with Gemini AI </button>
+                <button onClick={cancelAIDialog}>Cancel</button>
+            </dialog>
             <header className="caloriesDateNav">
                 <button onClick={() => setCurDate(new Date(curDate.setDate(curDate.getDate() - 1)))}>
                     &lt;
@@ -54,6 +118,9 @@ const CaloriesPage = () => {
             <AddFood mealType="Breakfast" curDate={formatDateToYYYYMMDD(curDate)} userId={userId} onFoodChange={handleFoodChange} />
             <AddFood mealType="Lunch" curDate={formatDateToYYYYMMDD(curDate)} userId={userId} onFoodChange={handleFoodChange} />
             <AddFood mealType="Dinner" curDate={formatDateToYYYYMMDD(curDate)} userId={userId} onFoodChange={handleFoodChange} />
+            <div id='AI_Scanner_ctn'>
+                <button onClick={() => setShowDialog(true)}>Scan with AI</button>
+            </div>
         </div>
     );
 }
