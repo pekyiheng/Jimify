b82ccb77f54c22b0727e482c046c680eede2ae7c
Fu Xiang <orfuxiang2015@gmail.com>
2025-07-17 15:57:25 +0800

    Updated activity log to show silent if no logs in past week

--- client/src/pages/MyProfilePage.jsx
+++ client/src/pages/MyProfilePage.jsx
@@ -134,10 +134,12 @@ const MyProfilePage = () => {
             <p>Activity Level: {activityLevel}</p>
             <p>Goal: {goal}</p>
             <h3>Activity Log for past week</h3>
-            <ul>
-                {activity.map((entry, index) => 
-                    (<li key={entry.id}><ActivityEntry activity={entry.note} time={entry.time}/></li>))}
-            </ul>
+            {activity.length == 0
+                ? (<p>Silent...</p>)
+                : (<ul>
+                    {activity.map((entry, index) => 
+                        (<li key={entry.id}><ActivityEntry activity={entry.note} time={entry.time}/></li>))}
+                </ul>)}
         </div>
     ) 
 }
